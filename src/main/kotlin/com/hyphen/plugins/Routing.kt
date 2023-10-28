4aef7f9a694f7d6d622f3f0a59f8354e1adf2165
Vivek <vivek.sharma@mpokket.com>
2023-10-28 16:11:44 +0530

    Root endpoint added

--- src/main/kotlin/com/hyphen/plugins/Routing.kt
+++ src/main/kotlin/com/hyphen/plugins/Routing.kt
@@ -1,13 +1,11 @@
 package com.hyphen.plugins
 
+import com.hyphen.routes.root
 import io.ktor.application.*
-import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        root()
     }
 }
